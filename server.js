037aecfec37695f85c749d6e0aff806541375a7b
Kamil <development.acc.1125@gmail.com>
2026-01-27 19:40:37 +0100

    port fixed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { Pool } = pkg;
 dotenv.config();
 
 const app = express();
-const PORT = process.env.NODE_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // __dirname replacement in ESM
 const __filename = fileURLToPath(import.meta.url);
